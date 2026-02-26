6668bd9da7cce27dee8a318bf14447583d83ce30
Vidhi <vidhidua707@gmail.com>
2026-02-26 20:13:42 +0530

    updated files.js and server.js

--- backend/routes/files.js
+++ backend/routes/files.js
@@ -1,23 +1,24 @@
 const express=require('express');
 const router=express.Router();
 const upload=require('../controllers/fileController');
-router.post('/upload',upload.single('file'),(req,res)=>{
+router.post('/upload',upload.array('file',5),(req,res)=>{
     try{
-        if(!req.file){
+        if(!req.files||req.files.length===0){
             return res.status(400).json({
                 success:false,
                 message:"No file uploaded",
             });
         }
+        const fileDetails=req.files.map(file=>({
+              filename:file.filename,
+                originalname:file.originalname,
+                mimetype:file.mimetype,
+                size:file.size,
+        }))
         res.status(200).json({
             success:true,
             message:"File uploaded successfully",
-            fileDetails:{
-                filename:req.file.filename,
-                originalname:req.file.originalname,
-                mimetype:req.file.mimetype,
-                size:req.file.size,
-            },
+            files:fileDetails,
         });
     }
     catch(error){
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-
+const fileRoutes = require("./routes/files");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api", fileRoutes);
 app.get("/", (req, res) => {
   res.send("Backend is running !!");
 });
