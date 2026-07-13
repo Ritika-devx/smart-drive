f41b15410e1cd0408316c43539a7daa52a495797
Ritika Rana <ranaritika730@gmail.com>
2026-07-13 10:47:44 +0530

    Completed search feature

--- backend/routes/suggestionsRoutes.js
+++ backend/routes/suggestionsRoutes.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const router = express.Router();
+
 const prisma = require("../utils/prismaClient");
 const authMiddleware = require("../middleware/authMiddleware");
 const { getSuggestions } = require("../services/optimizationService");
 
+console.log("Suggestions route loaded");
 
 router.get("/suggestions", authMiddleware, async (req, res) => {
   try {
     const files = await prisma.files.findMany();
+
     if (files.length === 0) {
-  return res.status(200).json({
-    success: true,
-    message: "No files available",
-    data: [],
-  });
-}
+      return res.status(200).json({
+        success: true,
+        data: [],
+      });
+    }
 
     const suggestions = files.map((file) => ({
       id: file.id,
@@ -28,10 +30,12 @@ router.get("/suggestions", authMiddleware, async (req, res) => {
     });
 
   } catch (error) {
+
     res.status(500).json({
       success: false,
       message: error.message,
     });
+
   }
 });
 
