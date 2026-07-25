966a0673c64e077d2e4ec588c659ec6d5a94d84a
Vidhi Dua <vidhidua707@gmail.com>
2026-07-25 17:12:43 +0530

    change in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,61 +1,61 @@
-// import { Routes, Route } from "react-router-dom";
-
-// import AppShell from "./AppShell";
-
-// import Login from "./pages/Login";
-// import Suggestions from "./pages/Suggestions";
-// import Upload from "./pages/Upload";
-
-// import ProtectedRoute from "./components/ProtectedRoute";
-
-// function App() {
-//   return (
-//     <Routes>
-
-//       {/* Login */}
-//       <Route path="/" element={<Login />} />
-
-//       {/* Dashboard */}
-//       <Route
-//         path="/dashboard"
-//         element={
-//           <ProtectedRoute>
-//             <AppShell />
-//           </ProtectedRoute>
-//         }
-//       />
-
-//       {/* Suggestions */}
-//       <Route
-//         path="/suggestions"
-//         element={
-//           <ProtectedRoute>
-//             <Suggestions />
-//           </ProtectedRoute>
-//         }
-//       />
-
-//       {/* Upload */}
-//       <Route
-//         path="/upload"
-//         element={
-//           <ProtectedRoute>
-//             <Upload />
-//           </ProtectedRoute>
-//         }
-//       />
-
-//     </Routes>
-//   );
-// }
-
-// export default App;
+import { Routes, Route } from "react-router-dom";
 
 import AppShell from "./appShell";
 
+import Login from "./pages/Login";
+import Suggestions from "./pages/Suggestions";
+import Upload from "./pages/Upload";
+
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
-  return <AppShell />;
+  return (
+    <Routes>
+
+      {/* Login */}
+      <Route path="/" element={<Login />} />
+
+      {/* Dashboard */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <AppShell />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Suggestions */}
+      <Route
+        path="/suggestions"
+        element={
+          <ProtectedRoute>
+            <Suggestions />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* Upload */}
+      <Route
+        path="/upload"
+        element={
+          <ProtectedRoute>
+            <Upload />
+          </ProtectedRoute>
+        }
+      />
+
+    </Routes>
+  );
 }
 
 export default App;
+
+// import AppShell from "./appShell";
+
+// function App() {
+//   return <AppShell />;
+// }
+
+// export default App;
 
